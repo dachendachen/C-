2bc1e7d606f500c77c382372f6fd41bb54f897ae
Chen <ninodachen@yahoo.com>
2020-06-03 18:57:30 -0400

    1.4.1 ; 1.4.2

--- chapter1.cpp
+++ chapter1.cpp
@@ -1,25 +1,27 @@
 /*
  C++ primer exercise
- Author: Chen
+ Chen Da
  */
 #include <iostream>
 
 int main(){
  
-    // demo
+    /*section 1.2 demo
     std::cout << "Enter two numbers: " << std::endl;
     int v1=0, v2 =0;
     std::cin >> v1 >> v2;
     std::cout << "The sum of "<< v1 <<" and "<< v2 <<" is "<< v1+v2 << std::endl;
-
+     */
     
-    //exercise 1.3 print hello world
+    /*exercise 1.3 print hello world
     std::cout << "Hello World!"<< std::endl;
+    */
     
-    //1.4 try *, multiplication
+    /*1.4 try *, multiplication
     std::cout << "the product is " << v1*v2<< std::endl;
+    */
     
-    //1.5 rewrite line 13 into separate statement to print each operand
+    /*1.5 rewrite line 13 into separate statement to print each operand
     std::cout << "the sum of ";
     std::cout << v1;
     std::cout << " and ";
@@ -27,6 +29,7 @@ int main(){
     std::cout << " is ";
     std::cout << v1+v2;
     std::cout << std::endl;
+    */
     
     /*1.6 determine whether the following program fragment is legal
     std::cout << "The sum of " << v1;
@@ -37,32 +40,60 @@ int main(){
      Compiler Error
      */
     
-    //1.4 flow of control
+    //section 1.4 flow of control
     
+    /*1.4.1 the  while statement, Demo
     int sum = 0, val= 1;
-    //keep executing the while as long as val is less than or equal to 10
     while(val<=10)
     {
         sum+= val;
         ++val;
     }
     std:: cout<< "Sum of 1 to 10 inclusive is " << sum << std::endl;
+    */
     
     /* exercise 1.9 while loop to sum numbers from 50 to 100
-     
+     int sum_9 = 0, val_9 = 50;
+     while(val_9 <=100)
+     {
+         sum_9 += val_9;
+         ++val_9;
+     }
+     std::cout<<"Sum of 50 to 100 inclusive is " << sum_9 << std::endl;
+     //Sum of 50 to 100 inclusive is 3825
      */
-    int sum_9 = 0, val_9 = 50;
-    while(val_9 <=100)
+
+    /* 1.10 prints the bumbers from ten down to zero, using decrement, --
+    int start = 10;
+    while(start>=0)
     {
-        sum_9 += val_9;
-        ++val_9;
+        std::cout << start << std::endl;
+        --start;
     }
-    std::cout<<"Sum of 50 to 100 inclusive is " << sum_9 << std::endl;
+    */
+    
+
+    /* 1.11 prompts the users for two integers. print each number in tha range specified by those two integers. assuming the first number always be smaller.
+
+    int input1=0, input2=0;
+    std::cout << "Enter two numbers" << std::endl;
+    std::cin >> input1 >> input2;
+    while(input1 <= input2){
+        std::cout<< input1 << std::endl;
+        ++input1;
+    }
+      */
+    
+    /* 1.4.2 the for statement
+     
+    int sum =0;
+    for(int val=1;val<=10; ++val)
+        sum+=val;
+    std::cout <<"Sum of 1 to 10 inclusive is " << sum <<std::endl;
+    */
     
     return 0;
-    
-    
-    
+
 }
 
 
